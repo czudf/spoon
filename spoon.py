f4877ad1d9545f33b3f32404c15b1a5f0678c514
guantao.gao <guantao.gao@clickzetta.com>
2025-08-29 22:11:47 +0800

    Add project

---
+++ spoon.py
@@ -0,0 +1,16 @@
+# Copyright 2025 Yunqi Inc
+# SPDX-License-Identifier: Apache-2.0
+
+__version__ = "0.0.1.dev0"
+
+
+def spoon():
+    print(f"Hello from spoon {__version__}!")
+
+
+def main():
+    spoon()
+
+
+if __name__ == "__main__":
+    main()
