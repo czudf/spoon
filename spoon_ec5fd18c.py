2f508a1cc017d00b9ed881b477360e80960ef48b
guantao.gao <guantao.gao@clickzetta.com>
2025-08-30 18:44:34 +0800

    Update workflows

--- spoon_ec5fd18c.py
+++ spoon_ec5fd18c.py
@@ -4,7 +4,7 @@
 import importlib.metadata
 
 try:
-    __version__ = importlib.metadata.version("spoon")
+    __version__ = importlib.metadata.version("spoon-ec5fd18c")
 except importlib.metadata.PackageNotFoundError:
     # Fallback if running from source without being installed
     __version__ = "0.0.0"
